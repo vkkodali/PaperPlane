1e677285dfdf1301849e6e154dfdaeabaee2317c
vkkodali <vkkodali@gmail.com>
2016-06-06 07:21:46 -0400

    Update PP_Script.js

--- PP_Script.js
+++ PP_Script.js
@@ -1,6 +1,6 @@
 var theUrl = document.getElementsByClassName("icons portlet")[0].getElementsByTagName("a")[0];
 if (theUrl.innerHTML.startsWith("<img alt=\"Icon for HighWire\"")) {
-  window.open(String(theUrl).replace("long","full.pdf"), "_self");
+  window.open(String(theUrl).replace('long', 'full.pdf'), "_self");
 } else {
   alert("Not a HighWire Journal");
 }
