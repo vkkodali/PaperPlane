7bc98cc631f04bd3867a246d0db78179b0e62a35
vkkodali <vkkodali@gmail.com>
2016-06-07 07:21:07 -0400

    Update PP_Script.js
    using window.location.href = ### does not add the page to browser's history; so can't go back to pubmed page from the PDF page
    Instead using window.location allows going back.

--- PP_Script.js
+++ PP_Script.js
@@ -1,6 +1,6 @@
 var theUrl = document.getElementsByClassName("icons portlet")[0].getElementsByTagName("a")[0];
 if (theUrl.innerHTML.startsWith("<img alt=\"Icon for HighWire\"")) {
-  window.location.href = String(theUrl).replace('long', 'full.pdf');
+  window.location = String(theUrl).replace('long', 'full.pdf');
 } else {
   alert("Not a HighWire Journal");
 }
