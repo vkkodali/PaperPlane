48d4f005c2447bb69ce8b3c1cae3b6d2bb124cc9
vkkodali <vkkodali@gmail.com>
2016-06-09 07:58:39 -0400

    Update PP_Script.js
    biomedcentral journals added.

--- PP_Script.js
+++ PP_Script.js
@@ -1,6 +1,9 @@
 var theUrl = document.getElementsByClassName("icons portlet")[0].getElementsByTagName("a")[0];
 if (theUrl.innerHTML.startsWith("<img alt=\"Icon for HighWire\"")) {
-  window.location = String(theUrl).replace('long', 'full.pdf');
-} else {
-  alert("Not a HighWire Journal");
+	window.location = String(theUrl).replace('long', 'full.pdf');
+} else if (String(theUrl).includes("biomedcentral.com")) {
+	theUrl = String(theUrl).replace('articles', 'track/pdf');
+	window.location = theUrl + '?site=pubmed.gov';
+}	else {
+	alert("The way to the pdf I know not.  Yes, hmmm.");
 }
