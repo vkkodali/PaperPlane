40fe585abb63a4764d1d78b86e1c80b5ebc5d6f4
Vamsi Kodali <vkkodali@gmail.com>
2016-07-22 08:20:33 -0400

    testing

--- PP_Script.js
+++ PP_Script.js
@@ -1,3 +1,4 @@
+/* testing */
 var theUrl = document.getElementsByClassName("icons portlet")[0].getElementsByTagName("a")[0];
 if (theUrl.innerHTML.startsWith("<img alt=\"Icon for HighWire\"")) {
 	window.location.href = String(theUrl).replace('long', 'full.pdf');
