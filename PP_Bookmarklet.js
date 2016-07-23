f06cbdea8e71c585b259cd526fba253f64a5c826
Vamsi Kodali <vkkodali@gmail.com>
2016-07-23 13:07:09 -0400

    testing v2 updates

--- PP_Bookmarklet.js
+++ PP_Bookmarklet.js
@@ -1,5 +1,5 @@
-javascript: (function () { 
-  var jsCode = document.createElement('script'); 
-  jsCode.setAttribute('src', 'http://rawgit.com/vkkodali/PaperPlane/master/PP_Script.js');
-	document.body.appendChild(jsCode); 
+javascript: (function () {
+  var jsCode = document.createElement('script');
+  jsCode.setAttribute('src', 'http://rawgit.com/vkkodali/PaperPlane/PP_testing/PP_Script.js');
+	document.body.appendChild(jsCode);
  }());
--- PP_Script.js
+++ PP_Script.js
@@ -1,3 +1,16 @@
+/* display a random alert message */
+function noPDFErrorMessage () {
+  var yodaSays = [
+    "Where the pdf is I know not.",
+    "The way to the PDF I know not. Yes, hmmm.",
+    "Sorry, seem to locate the pdf, I cannot.",
+    "No clue where the pdf is, I have.",
+    "Sorry, locate the pdf I cannot.  Yeesssssss."
+  ];
+  var i = Math.floor(Math.random()*yodaSays.length);
+  alert(yodaSays[i]);
+}
+
 var urlPatterns = [
   {urlIn: 'biomedcentral.com', replaceThis: 'articles', replaceWith: 'track/pdf', addThis: '?site=pubmed.gov'},
   {urlIn: 'endocrine.org', replaceThis: 'doi', replaceWith: 'doi/pdf', addThis: ''},
@@ -13,19 +26,33 @@ var urlPatterns = [
   {urlIn: 'dx.doi.org/10.1111', replaceThis: 'dx.doi.org', replaceWith: 'onlinelibrary.wiley.com/doi', addThis: '/pdf'},
   {urlIn: 'dx.doi.org/10.1113', replaceThis: 'dx.doi.org', replaceWith: 'onlinelibrary.wiley.com/doi', addThis: '/pdf'},
   {urlIn: 'dx.doi.org/10.1089', replaceThis: 'dx.doi.org', replaceWith: 'online.liebertpub.com/doi/pdf', addThis: ''},
-  {urlIn: , replaceThis: , replaceWith: , addThis: }
-
+  {urlIn: 'dx.doi.org/10.1007', replaceThis: 'dx.doi.org', replaceWith: 'link.springer.com/content/pdf/', addThis: '.pdf'},
+  {urlIn: 'dx.doi.org/10.1172', replaceThis: 'dx.doi.org/10.1172/JCI', replaceWith: 'jci.org/articles/view/', addThis: '/pdf/render'},
+  {urlIn: 'karger.com/?DOI=', replaceThis: '?DOI=', replaceWith: 'Article/Pdf/', addThis: ''}
 ];
 
-/* var theUrl = String(document.getElementsByClassName("icons portlet")[0].getElementsByTagName("a")[0]);
-var garbage = theUrl.indexOf('?');
-theUrl = theUrl.substring(0, garbage != -1 ? garbage : theUrl.length); */
+if (!(window.location.href.startsWith('http://www.ncbi.nlm.nih.gov/pubmed/'))) {
+  alert("This script works only on PubMed abstract pages");
+  return;
+} else if (document.getElementsByClassName("icons portlet").length < 1) {
+  alert("It appears that a Full text link is not available through PubMed.");
+  return;
+};
 
-for (i = 0; i < urlPatterns.length; i += 1) {
-  if (theUrl.includes(urlPatterns[i].urlIn)) {
-    console.log(theUrl.replace(urlPatterns[i].replaceThis, urlPatterns[i].replaceWith) + urlPatterns[i].addThis);
-    break;
-  } else {
-    console.log("keep going...");
+var theUrl = String(document.getElementsByClassName("icons portlet")[0].getElementsByTagName("a")[0]);
+if (theUrl.innerHTML.startsWith("<img alt=\"Icon for HighWire\"")) {
+  window.open(String(theUrl).replace('long', 'full.pdf'), "_self");
+} else if {
+  var garbage = theUrl.indexOf('?');
+  theUrl = theUrl.substring(0, garbage != -1 ? garbage : theUrl.length);
+  for (i = 0; i < urlPatterns.length; i += 1) {
+    if (theUrl.includes(urlPatterns[i].urlIn)) {
+      window.open(theUrl.replace(urlPatterns[i].replaceThis, urlPatterns[i].replaceWith) + urlPatterns[i].addThis, "_self");
+      return;
+    }
   }
-}
+} else if (String(document.getElementsByClassName("rprtid")[0].getElementsByTagName("a")[1]).includes("pmc/articles")) {
+  window.open(document.getElementsByClassName("rprtid")[0].getElementsByTagName("a")[1] + 'pdf', "_self");
+} else {
+  noPDFErrorMessage();
+};
