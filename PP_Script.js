1c9438a7f00a5d7b2bf78fab18909d4152c0a8ff
Vamsi Kodali <vkkodali@gmail.com>
2016-07-26 08:58:28 -0400

    testing v2

--- PP_Script.js
+++ PP_Script.js
@@ -35,7 +35,7 @@ function patternFetch () {
 }
 
 function lastDitchFetch () {
-  var theUrl = String(document.getElementsByClassName("icons portlet")[0].getElementsByTagName("a")[0]);
+  var theUrl = document.getElementsByClassName("icons portlet")[0].getElementsByTagName("a")[0];
   if (theUrl.innerHTML.startsWith("<img alt=\"Icon for HighWire\"")) {
     window.open(String(theUrl).replace('long', 'full.pdf'), "_self");
   } else if (String(document.getElementsByClassName("rprtid")[0].getElementsByTagName("a")[1]).includes("pmc/articles")) {
