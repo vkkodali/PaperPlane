672c340ae2b5526057c569681544f7b4357c8412
Vamsi Kodali <vkkodali@gmail.com>
2016-07-28 10:28:44 -0400

    testing v2

--- PP_Script.js
+++ PP_Script.js
@@ -34,7 +34,7 @@ function reformatPlosUrls () {
   ];
   for (i = 0; i < PLoSJournals.length; i += 1) {
     if (String(theUrl).includes(PLoSJournals[i].shortName)) {
-      theUrl = String(theUrl).replace('dx.plos.org', 'journals.plos.org/' + PLoSJournals[i].longName + '/article?id=');
+      window.open(String(theUrl).replace('dx.plos.org', 'journals.plos.org/' + PLoSJournals[i].longName + '/article?id='), "_self");
       return;
     }
   }
