9adf46a6b9fbcd8e9ea4e014c71d1db70157f4d6
Vamsi Kodali <vkkodali@gmail.com>
2016-08-06 15:37:28 -0400

    Update PP_Bookmarklet.js

--- PP_Bookmarklet.js
+++ PP_Bookmarklet.js
@@ -2,4 +2,4 @@ javascript: (function () {
   var jsCode = document.createElement('script');
   jsCode.setAttribute('src', 'http://rawgit.com/vkkodali/PaperPlane/PP_testing/PP_Script.js');
 	document.body.appendChild(jsCode);
- }());
+ });
