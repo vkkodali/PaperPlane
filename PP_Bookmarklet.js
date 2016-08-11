ec6d575f39428762323bfce33ee698184077c0c1
Vamsi Kodali <vkkodali@gmail.com>
2016-08-11 12:08:22 -0400

    Can resolve when invoked from PubMed https links

--- PP_Bookmarklet.js
+++ PP_Bookmarklet.js
@@ -1,5 +1,5 @@
 javascript: (function () {
   var jsCode = document.createElement('script');
-  jsCode.setAttribute('src', 'http://rawgit.com/vkkodali/PaperPlane/master/PP_Script.js');
+  jsCode.setAttribute('src', 'https://rawgit.com/vkkodali/PaperPlane/master/PP_Script.js');
 	document.body.appendChild(jsCode);
  }());
